3d149ab4de451dbf7531127c8f4998257dc5bdf2
unknown <coreyfuller00@gmail.com>
2020-07-03 13:49:34 -0400

    modified downloader

--- downloader.ps1
+++ downloader.ps1
@@ -5,13 +5,13 @@ Astronomy Picture of the day to test functionality -Eli
 #>
 
 #File path to the folder where you want files stored -Eli
-$PATH_VAR = "C:\Users\xaubr\OneDrive\Desktop\E-pics\nasa.jpg"
+#$PATH_VAR = "C:\Users\xaubr\OneDrive\Desktop\E-pics\nasa.jpg"
 
 #Place holder API KEY -Eli
-$API_KEY = "Cf1jJAX2OXflpgkj3nBdcVCdJwN7HpAg1yjUxqFg"
+#$API_KEY = "Cf1jJAX2OXflpgkj3nBdcVCdJwN7HpAg1yjUxqFg"
 
 #This will be used to hold the return of Invoke-Web-Request -Eli
-$RETURN_VALUE
+#$RETURN_VALUE
 
 <#CJ and I thought about using Invoke-RestMethod, but realized
 it does not return the http status code so we returned to the
@@ -25,22 +25,35 @@ $RETURN_VALUE.StatusCode
 
 wget $RETURN_VALUE.url -outfile "C:\Users\xaubr\OneDrive\Desktop\E-pics\nasa.jpg"#>
 
-$RETURN_VALUE = Invoke-WebRequest -Uri https://api.nasa.gov/planetary/apod?api_key=$API_KEY -Method Get
+#$RETURN_VALUE = Invoke-WebRequest -Uri https://api.nasa.gov/planetary/apod?api_key=$API_KEY -Method Get
 
 <#Although Invoke-WebRequest gives us the StatusCode member
 it does not natively return just the JSON object like Invoke-RestMethod
 So I had to use ConvertFrom-Json as a workaround to access the file URL -Eli
 #>
-$JSON_Object = ConvertFrom-Json $RETURN_VALUE
+#$JSON_Object = ConvertFrom-Json $RETURN_VALUE
 
 <#This downloads the file and places it in the folder
 The folder must already exist when this command is called -Eli
 #>
-wget $JSON_Object.url -OutFile $PATH_VAR
+#wget $JSON_Object.url -OutFile $PATH_VAR
+
+$Header = @{
+    'Authorization' = 'key=Eepv+BdqqMjFKIY7CUsL93dp4ILhhyrurjiQLuysjfu6D2PhhA=='
+    }
+    $Header.Values
+$testURI = 'https://squid-fortress-staging.eleostech.com/api/v1/documents/queued/next'
+$Header.GetType();
+$Header.ToString()
+#$var = Invoke-RestMethod -Method GET -URI 'https://squid-fortress-staging.eleostech.com/api/v1/documents/queued/next' -Headers $Header
+$var = Invoke-WebRequest -URI $testURI 
+$var = Invoke-WebRequest 'https://squid-fortress-staging.eleostech.com/api/v1/documents/queued/next'  -Headers $Header.ToString() -Method GET
 
 
 
 #Need API key from Drive Axle Hub
+
+
 
 ## Script Structure to Implement - Corey
 
