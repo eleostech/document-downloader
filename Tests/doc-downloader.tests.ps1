19deb80620f923b551d57c21a5c22df97afb9256
Corey Fuller <coreyfuller00@gmail.com>
2020-09-21 18:12:08 -0400

    Removing Drive Axle headers

--- Tests/doc-downloader.tests.ps1
+++ Tests/doc-downloader.tests.ps1
@@ -1,8 +1,10 @@
 $here = (Split-Path -Parent $MyInvocation.MyCommand.Path)
 $src = Split-Path -Path $here -Parent
 . $src\doc-downloader.functions.ps1
+$proj = Split-Path -Path $src -Parent
 
-$BASE_URI = 'https://359c4f0b-d5b7-459f-9997-dbebf9369b15.mock.pstmn.io'
+
+$BASE_URI = 'https://localhost:44373'
 
 $testfile = ($here + "\LogFile.txt")
 
@@ -10,16 +12,12 @@ if((Test-Path $testfile) -ne $True){
     New-Item -Path $testfile -ItemType File
 }
 
-$DRIVE_AXLE = $false # If Drive Axle Hub Customer - this value should be $true, otherwise $false
 $API_KEY = "Placeholder"
-
-$DRIVE_AXLE_HEADERS = @{ Authorization = ("driveaxle=" + $API_KEY) 
-                         Accept = 'application/json'}
 
 $ELEOS_HEADERS = @{ Authorization = ("key=" + $API_KEY)
                     Accept = 'application/json'}
 
-$HEADERS = If ($DRIVE_AXLE) { $DRIVE_AXLE_HEADERS } Else { $ELEOS_HEADERS }
+$HEADERS =  $ELEOS_HEADERS
 
 Describe "Helper Function Tests" {
     Context 'Verifying helper functions produce correct output' {
@@ -58,16 +56,27 @@ Describe "Consume API Function Tests" {
     Context 'Testing GetNextDoc function' {
         it 'GetNextDoc should return a 302 if there is a document in the queue' {
             $request = $BASE_URI + '/api/v1/documents/queued/next'
-           # $response = GetNextDoc $request $HEADERS $testfile
-           # $response.StatusCode | should be 302
-           $true | Should be $true
+            $response = GetNextDoc $request $HEADERS $testfile
+            $response.StatusCode | should be 302
         }
         it 'GetNextDoc should return a 304 if there is not a document in the queue' {
-            $request = $BASE_URI + '/api/v2/documents/queued/next'
-            #$response = GetNextDoc $request $HEADERS $testfile
-            #$response.StatusCode | should not be 304
-            $true | Should be $true
+            $request = $BASE_URI + '/api/v1/documents/queued/next/empty'
+            $response = GetNextDoc $request $HEADERS $testfile
+            $response.StatusCode | should be 304
         }
+
+        it 'GetNextDoc should throw an expection if a 404 status code is returned' {
+            $request = $BASE_URI + '/api/v1/documents/queued/next/fail'
+            $exception = $false
+            try {
+                $response = GetNextDoc $request $HEADERS $testfile
+            }
+            catch {
+                $exception = $_.CategoryInfo.Reason -eq 'WebException' 
+            }
+            $exception | Should be $true
+        }
+
 
         it 'GetNextDoc should throw an exception if a 500 status code is returned' {
             $request  =  $BASE_URI + '/api/v1/documents/queued/next/badserver'
@@ -83,13 +92,27 @@ Describe "Consume API Function Tests" {
     }
 
     Context 'Testing GetDocFromQueue function' {
-        it 'GetDocFromQueue should return a 302 with URL and filename to download document' {
+        it 'GetDocFromQueue should return a 200 with a URL to download document' {
             $request = $BASE_URI + '/api/v1/documents/queued/1'
-            # $response = GetDocFromQueue $request $HEADERS $testfile
-            # $response | should not be $null
-            $true | Should be $true
+            $response = GetDocFromQueue $request $HEADERS $testfile
+            $response.StatusCode | should be 200
+            $response = $response | ConvertFrom-Json
+            $response.downloadUrl | should not be $null
         }
+
         it 'GetDocFromQueue should throw an exception if a 404 status code is returned' {
+            $request = $BASE_URI + '/api/v1/documents/queued/2'
+            $exception = $false
+            try{
+                $request = GetNextDoc $request $HEADERS $testfile -ErrorAction SilentlyContinue 
+            } 
+            catch {
+                $exception = $_.CategoryInfo.Reason -eq 'WebException' 
+            }
+            $exception | Should be $true
+        }
+
+        it 'GetDocFromQueue should throw an exception if a 500 status code is returned' {
             $request = $BASE_URI + '/api/v1/documents/queued/2'
             $exception = $false
             try{
@@ -104,14 +127,13 @@ Describe "Consume API Function Tests" {
 
     Context 'Testing RemoveDocFromQueue function' {
         it 'RemoveDocFromQueue should return a 200 if a document was successfully removed from the queue' {
-            $request = $BASE_URI + '/api/v1/documents/queued/'
-            # $response = RemoveDocFromQueue $request $HEADERS $testfile
-            # $response = $response | ConvertFrom-Json
-            # $response.message | Should be "Document Downloaded Successfully"
-            $true | Should be $true
+            $request = $BASE_URI + '/api/v1/documents/queued/1'
+            $response = RemoveDocFromQueue $request $HEADERS $testfile
+            $response = $response | ConvertFrom-Json
+            $response.message | Should be "Document deleted successfully."
         }
-        it 'RemoveDocFromQueue should return a 404 if the document to be removed could not be found' {
-            $request = $BASE_URI + '/api/v1/documents/queued/1000000000'
+        it 'RemoveDocFromQueue should return null if the document to be removed could not be found' {
+            $request = $BASE_URI + '/api/v1/documents/queued/2'
             $response = RemoveDocFromQueue $request $HEADERS $testfile
             $response | should be $null
         }
@@ -119,7 +141,7 @@ Describe "Consume API Function Tests" {
     
     Context 'Testing ExponentialDeleteRetry function' {
         it 'ExpWait should return a null if the document is never removed after the multiple retires due to 404 not found' {
-            $request = $BASE_URI + '/api/v1/documents/queued/100000000000'
+            $request = $BASE_URI + '/api/v1/documents/queued/3'
             $response = ExponentialDeleteRetry $request $Headers $testfile
             $response | should be $null
         }
