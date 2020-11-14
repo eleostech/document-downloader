f4711ed038c528f50f8c294383006d6700de080d
Corey Fuller <coreyfuller00@gmail.com>
2020-11-14 13:28:53 -0500

    Changing Https Redirection and lauchSettings

--- MockServer/MockServer/Startup.cs
+++ MockServer/MockServer/Startup.cs
@@ -36,7 +36,7 @@ namespace MockServer
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
